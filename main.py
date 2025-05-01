1e6288b306a4009ee2df47b7068de5f095c37f16
Rodrigo Junio <RodrigoJunio@Rodrigo-Junio.local>
2025-04-30 22:55:44 -0300

    Corrige binding da porta e host para deploy no Render

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# main.py
+
 from flask import Flask, render_template, request
 from consulta import diagnostico_cnpj
 from registro import registrar_cnpj
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +21,5 @@ def registro():
     return registrar_cnpj(request)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
